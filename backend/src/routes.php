8572afbfe5c1d540c29d7a6b59a87cbb5ccd255a
giulioz <mail.zausa.giulio@gmail.com>
2019-01-12 16:00:25 +0100

    WIP

--- backend/src/routes.php
+++ backend/src/routes.php
@@ -2,41 +2,66 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
-use ReallySimpleJWT\Token;
+use \Firebase\JWT\JWT;
 
-function validate($request) {
-    $headers = $request->getHeaders();
-    $token = explode(" ", $headers["HTTP_AUTHORIZATION"][0])[1];
-    if ($token == null) {
+const secret = "abc";
+
+function getUser($request) {
+    try {
+        $headers = $request->getHeaders();
+        $token = explode(" ", $headers["HTTP_AUTHORIZATION"][0])[1];
+        $decoded = JWT::decode($token, secret, array('HS256'));
+        return decoded;
+    } catch (Exception $e) {
         return false;
     }
-    $result = Token::validate($token, 'secret');
 }
+
+
+
+// check user and returns token
+$app->post('/login', function ($request, $response, $args) {
+    $input = $request->getParsedBody();
+    $sth = $this->db->prepare("SELECT u.Nome, u.Cognome, u.Indirizzo, u.Telefono, u.Login, u.Amministratore
+    FROM Utenti u
+    WHERE u.Login = :user AND u.Password = :password");
+    $sth->bindParam("user", $input["user"]);
+    $sth->bindParam("password", $input["password"]);
+    $sth->execute();
+    $users = $sth->fetchAll();
+
+    if (count($users) <= 0) {
+        return $response->withStatus(401);
+    }
+
+    $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+    $payload = json_encode($users[0]);
+    $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+    $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+    $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, secret, true);
+    $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+    $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+    return $jwt;
+});
 
 // add new user
 $app->post('/users', function ($request, $response, $args) {
-    $sth = $this->db->prepare("SELECT * FROM Utenti");
+    $input = $request->getParsedBody();
+    $sth = $this->db->prepare("CALL `AddUser`(:Nome, :Cognome, :Indirizzo, :Telefono, :Login, :Password);");
+    $sth->bindParam("Nome", $input["Nome"]);
+    $sth->bindParam("Cognome", $input["Cognome"]);
+    $sth->bindParam("Indirizzo", $input["Indirizzo"]);
+    $sth->bindParam("Telefono", $input["Telefono"]);
+    $sth->bindParam("Login", $input["Login"]);
+    $sth->bindParam("Password", $input["Password"]);
     $sth->execute();
-    $users = $sth->fetchAll();
-    return $this->response->withJson($users);
+    return 200;
 });
-
-// check user and returns token
-$app->get('/login', function ($request, $response, $args) {
-    $sth = $this->db->prepare("SELECT * FROM Utenti");
-    $sth->execute();
-    $users = $sth->fetchAll();
-    return $this->response->withJson($users);
-});
-
-
-
-
-
 
 // get all users
 $app->get('/users', function ($request, $response, $args) {
-    if (!validate($request)) {
+    if (!getUser($request)->Amministratore) {
         return $response->withStatus(401);
     }
 
@@ -70,10 +95,57 @@ $app->get('/pizzas', function ($request, $response, $args) {
 
 // get all orders
 $app->get('/orders', function ($request, $response, $args) {
-    $sth = $this->db->prepare("SELECT * FROM Ordini");
-    $sth->execute();
-    $orders = $sth->fetchAll();
-    return $this->response->withJson($orders);
+    $user = getUser($request);
+
+    if ($user->Amministratore) {
+        $sth = $this->db->prepare("SELECT * FROM Ordini");
+        $sth->execute();
+        $orders = $sth->fetchAll();
+        return $this->response->withJson($orders);
+    } else if ($user) {
+        $sth = $this->db->prepare("SELECT * FROM Ordini o WHERE o.Utente = :user");
+        $sth->bindParam("user", $user->Login);
+        $sth->execute();
+        $orders = $sth->fetchAll();
+        return $this->response->withJson($orders);
+    } else {
+        return $response->withStatus(401);
+    }
+});
+
+// add a new order
+$app->post('/orders', function ($request, $response, $args) {
+    $user = getUser($request);
+    
+    if ($user) {
+        $sth = $this->db->prepare("INSERT INTO Ordini()");
+        $sth->bindParam("user", $user->Login);
+        $sth->execute();
+        $orders = $sth->fetchAll();
+        return $this->response->withJson($orders);
+    } else {
+        return $response->withStatus(401);
+    }
+});
+
+// delete an order
+$app->delete('/orders/[{id}]', function ($request, $response, $args) {
+    $user = getUser($request);
+    
+    if ($user->Amministratore) {
+        $sth = $this->db->prepare("DELETE o FROM Ordini o WHERE o.ID_Ordine = :id");
+        $sth->bindParam("id", $args['id']);
+        $sth->execute();
+        return 200;
+    } else if ($user) {
+        $sth = $this->db->prepare("DELETE o FROM Ordini o WHERE o.Utente = :user AND o.ID_Ordine = :id");
+        $sth->bindParam("user", $user->Login);
+        $sth->bindParam("id", $args['id']);
+        $sth->execute();
+        return 200;
+    } else {
+        return $response->withStatus(401);
+    }
 });
 
 // get all ingredients
