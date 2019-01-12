7baa5e4fecbf29f7c4860561de3478e564ba3dc3
giulioz <mail.zausa.giulio@gmail.com>
2019-01-12 12:53:05 +0100

    routes

--- backend/src/routes.php
+++ backend/src/routes.php
@@ -16,22 +16,36 @@ $app->get('/users', function ($request, $response, $args) {
 $app->get('/pizzas', function ($request, $response, $args) {
     $sth = $this->db->prepare("SELECT * FROM Pizze");
     $sth->execute();
-    $users = $sth->fetchAll();
-    return $this->response->withJson($users);
+    $pizzas = $sth->fetchAll();
+
+    for ($i=0; $i < count($pizzas); $i++) {
+        $sth1 = $this->db->prepare("SELECT ip.NomeIngrediente FROM Ing_Pizze ip WHERE ip.NomePizza=:name");
+        $sth1->bindParam("name", $pizzas[$i]["Nome"]);
+        $sth1->execute();
+        $ingredients = $sth1->fetchAll();
+
+        function ext($n) {
+            return($n["NomeIngrediente"]);
+        }
+        $tmp = array_map("ext", $ingredients);
+        $pizzas[$i]["Ingredienti"] = $tmp;
+    }
+    
+    return $this->response->withJson($pizzas);
 });
 
 // get all orders
 $app->get('/orders', function ($request, $response, $args) {
     $sth = $this->db->prepare("SELECT * FROM Ordini");
     $sth->execute();
-    $users = $sth->fetchAll();
-    return $this->response->withJson($users);
+    $orders = $sth->fetchAll();
+    return $this->response->withJson($orders);
 });
 
 // get all ingredients
 $app->get('/ingredients', function ($request, $response, $args) {
     $sth = $this->db->prepare("SELECT * FROM Ingredienti");
     $sth->execute();
-    $users = $sth->fetchAll();
-    return $this->response->withJson($users);
+    $ingredients = $sth->fetchAll();
+    return $this->response->withJson($ingredients);
 });
